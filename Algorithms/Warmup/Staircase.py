0ac9c4f6eab334b01a382e62475c48cf763cdddc
Josh Penn-Pierson <jpennpierson@gmail.com>
2016-11-30 15:43:26 +1300

    algorithm optimization

--- Algorithms/Warmup/Staircase.py
+++ Algorithms/Warmup/Staircase.py
@@ -8,9 +8,5 @@ import sys
 n = int(input().strip())
 
 for i in range(1,n+1):
-    string = ""
-    for a in range(n-i):
-        string += " "
-    for b in range(i):
-        string += "#"
+    string = " "*(n-i) + "#"*i
     print(string)
